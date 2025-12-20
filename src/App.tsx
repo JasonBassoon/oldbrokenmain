f4541e6a140e9a0f7ea1d58843b2eb783e3ee793
Jason Ettinger <spamforacautiousman@gmail.com>
2025-12-20 14:15:03 -0600

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import Hero from './components/Hero'
 import Projects from './components/Projects'
 import Learning from './components/Learning'
-import SOCLabTracker from './components/SOCLabTracker'
 import LifeExperiences from './components/LifeExperiences'
 import Contact from './components/Contact'
 
@@ -31,7 +30,6 @@ function App() {
             <a href="#hero" onClick={handleNavClick}>Home</a>
             <a href="#projects" onClick={handleNavClick}>Projects</a>
             <a href="#learning" onClick={handleNavClick}>Learning</a>
-            <a href="#soc-lab" onClick={handleNavClick}>SOC Lab</a>
             <a href="#experience" onClick={handleNavClick}>Experience</a>
             <a href="#contact" onClick={handleNavClick}>Contact</a>
           </div>
@@ -44,11 +42,6 @@ function App() {
         </div>
         <Projects />
         <Learning />
-        <section id="soc-lab" className="section">
-          <div className="container">
-            <SOCLabTracker />
-          </div>
-        </section>
         <LifeExperiences />
         <Contact />
       </main>
